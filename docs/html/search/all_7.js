84622a7303737f2e8a08c52926f8383b55fea20d
Noah LaCour-Thistle <noahlacour@users.noreply.github.com>
2025-05-06 15:57:56 -0230

    Update Doxygen documentation

--- docs/html/search/all_7.js
+++ docs/html/search/all_7.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['region_0',['region',['../structConfig.html#ad57d0ba99677f8cbb4bce87d6313d499',1,'Config']]]
+  ['readme_2emd_0',['README.md',['../README_8md.html',1,'']]],
+  ['region_1',['region',['../structConfig.html#ad57d0ba99677f8cbb4bce87d6313d499',1,'Config']]]
 ];
